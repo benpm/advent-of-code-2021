b166f889702b7a891ab919b3b4e6ac629c2bb7b9
benpm <schwalegos@gmail.com>
2021-12-02 23:02:20 -0700

    better boilerplate

--- boilerplate.py
+++ boilerplate.py
@@ -1,14 +1,41 @@
 from aocd import lines, submit
 
-ans = 0
+PART = 0
+EXAMPLE = True
 
-example = True
-
-if example:
+if EXAMPLE:
     with open("test.txt", "r") as f:
         lines = f.readlines()
 
-for line in [l.strip() for l in lines if l]:
-    print(line)
+lines = [l.strip() for l in lines if l != "\n"]
 
-#submit(ans)
+
+# ---------------------------------------------------------------------------
+# Part A
+# ---------------------------------------------------------------------------
+
+
+def part_A():
+    for l in lines:
+        print(l)
+    return 0
+
+
+# ---------------------------------------------------------------------------
+# Part B
+# ---------------------------------------------------------------------------
+
+
+def part_B():
+    for l in lines:
+        print(l)
+    return 0
+
+
+# ---------------------------------------------------------------------------
+
+
+ans = part_A() if PART == 0 else part_B()
+print("ANSWER:", ans)
+if not EXAMPLE and ans != 0:
+    submit(ans)
