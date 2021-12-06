66845e13769b3d9e84a271b1f4116c52317696dc
benpm <schwalegos@gmail.com>
2021-12-05 23:41:32 -0700

    day 6

--- original_solutions/day_06/day_06.py
+++ original_solutions/day_06/day_06.py
@@ -1,6 +1,7 @@
 from aocd import lines, submit
 import numpy as np
 from sys import argv
+from collections import defaultdict
 
 assert len(argv) == 4, "args: [part A/B] [example? t/f] [submit? t/f]"
 assert argv[1] in ['A', 'B'], "part must be A or B"
@@ -14,15 +15,10 @@ SUBMIT = (argv[3] == "t")
 if EXAMPLE:
     with open("test.txt", "r") as f:
         lines = f.readlines()
-    data = "\n".join(lines)
 
 lines = [l.strip() for l in lines]
-
 if lines[-1] == "":
     lines = lines[:-1]
-if data[-1] == "\n":
-    data = data[:-1]
-
 
 # ---------------------------------------------------------------------------
 # Part A
@@ -30,9 +26,16 @@ if data[-1] == "\n":
 
 
 def part_A():
-    for l in lines:
-        print(l)
-    return 0
+    fs = [int(f) for f in lines[0].split(",")]
+    for i in range(80):
+        toi = len(fs)
+        for i in range(toi):
+            fs[i] = (fs[i] - 1)
+            if fs[i] < 0:
+                fs[i] = 6
+                fs.append(8)
+    print(len(fs))
+    return len(fs)
 
 
 # ---------------------------------------------------------------------------
@@ -41,15 +44,30 @@ def part_A():
 
 
 def part_B():
-    for l in lines:
-        print(l)
-    return 0
+    fs = defaultdict(int)
+    for n in lines[0].split(","):
+        fs[(int(n) - 1) % 7] += 1
+    fish = len(lines[0].split(","))
+    sfs = defaultdict(int)
+    for i in range(255):
+        sfsc = sfs.copy()
+        for n, occ in fs.items():
+            if occ > 0 and (i) % 7 == n:
+                fish += occ
+                sfs[(i) % 9] += occ
+        for n, occ in sfsc.items():
+            if occ > 0 and (i) % 9 == n:
+                fish += occ
+                fs[(i) % 7] += occ
+
+    return fish
+
 
 
 # ---------------------------------------------------------------------------
 
 
-ans = part_A() if PART == 0 else part_B()
+ans = part_A() if PART == "A" else part_B()
 print("ANSWER:", ans)
 if SUBMIT and not EXAMPLE and ans != 0:
     submit(ans)
