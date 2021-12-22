87f82d4ece26eb915de295c36bcc8c9a68f94926
benpm <schwalegos@gmail.com>
2021-12-22 13:51:10 -0700

    day 10

--- original_solutions/day_10/day_10.py
+++ original_solutions/day_10/day_10.py
@@ -13,7 +13,7 @@ PART = argv[1]
 EXAMPLE = (argv[2] == "t")
 SUBMIT = (argv[3] == "t")
 
-lines = get_data(day=0,year=2021).splitlines()
+lines = get_data(day=10,year=2021).splitlines()
 
 if EXAMPLE:
     with open("test.txt", "r") as f:
@@ -30,11 +30,26 @@ if lines[-1] == "":
 # Part A
 # ---------------------------------------------------------------------------
 
+lc = ["[", "{", "<", "("]
+mp = {c:i for i,c in enumerate(lc)}
+rc = ["]", "}", ">", ")"]
+mp.update({c:i for i,c in enumerate(rc)})
+pts = {")":3, "]":57, "}":1197, ">":25137}
 
 def part_A():
+    ans = 0
     for l in lines:
-        print(l)
-    return 0
+        st = []
+        for c in l:
+            if c in lc:
+                st.append(c)
+            else:
+                if mp[c] == mp[st[-1]]:
+                    st.pop()
+                else:
+                    ans += pts[c]
+                    break
+    return ans
 
 
 # ---------------------------------------------------------------------------
@@ -43,15 +58,32 @@ def part_A():
 
 
 def part_B():
+    scrs = []
+    pts = {"(":1, "[":2, "{":3, "<":4}
     for l in lines:
-        print(l)
-    return 0
+        st = []
+        scr = 0
+        for c in l:
+            if c in lc:
+                st.append(c)
+            else:
+                if mp[c] == mp[st[-1]]:
+                    st.pop()
+                else:
+                    break
+        else:
+            # Complete line
+            for c in reversed(st):
+                scr = (scr * 5) + pts[c]
+            scrs.append(scr)
+    scrs = np.array(scrs)
+    return np.median(scrs)
 
 
 # ---------------------------------------------------------------------------
 
 
-ans = part_A() if PART == "A" else part_B()
+ans = int(part_A() if PART == "A" else part_B())
 print("ANSWER:", ans)
 if SUBMIT and not EXAMPLE and ans != 0:
     submit(ans)
